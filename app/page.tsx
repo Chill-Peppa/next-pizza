5021b3fefeabeab07a65503096c1119e287e41cb
Anastasia <www.nastya97@yandex.ru>
2024-08-27 18:14:41 +0300

    feature/main: верстка фильтрации

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 
       <TopBar />
 
-      <Container className="pb-14">
+      <Container className="pb-14 mt-10">
         <div className="flex gap-[60px]">
           {/* Фильтрация */}
           <div className="w-[250px]">
